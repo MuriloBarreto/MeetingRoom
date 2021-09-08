0c5eddec4bb3c5350161aea8f423d8e055bfcbb6
Murilo Barreto <barreto_11almeida@hotmail.com>
2021-09-07 21:18:15 -0300

    Criação do frontend e backend de uma sala de reuniao

---
+++ backend/src/main/java/com/example/salareunioes/SalareunioesApplication.java
@@ -0,0 +1,13 @@
+package com.example.salareunioes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalareunioesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalareunioesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/salareunioes/SalareunioesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.salareunioes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalareunioesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
